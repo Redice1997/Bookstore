7b5b88331d649f7ab2a0cc194614b21c13636489
Redice1997 <roshin-vladimir@mail.ru>
2022-04-16 21:19:50 +0300

    Добавить реализацию репозитория книг в памяти

--- Bookstore/IBookRepository.cs
+++ Bookstore/IBookRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Bookstore
 {
-    interface IBookRepository
+    public interface IBookRepository
     {
         Book[] GetAllByTitle(string titlePart);
     }
