9a80ea54522b5a3a8fef21684cc1910fc22bca54
Redice1997 <roshin-vladimir@mail.ru>
2022-04-20 22:03:30 +0300

    Добавить новую логику построения заказаю

--- Presentation/Store.Web/Models/Cart.cs
+++ Presentation/Store.Web/Models/Cart.cs
@@ -7,6 +7,7 @@ namespace Store.Web.Models
 {
     public class Cart
     {
+
         public IDictionary<int, int> Items { get; set; } = new Dictionary<int, int>();
 
         public decimal Amount { get; set; }
