190cfa6d61cd1956b88f92714d2401713a0f0a4b
Redice1997 <roshin-vladimir@mail.ru>
2022-05-21 22:32:58 +0300

    Реализовать оплату наличными

--- Bookstore/Order.cs
+++ Bookstore/Order.cs
@@ -9,12 +9,13 @@ namespace Bookstore
         public int Id { get; } 
         public string CellPhone { get; set; }
         public OrderDelivery Delivery { get; set; }
+        public OrderPayment Payment { get; set; }
 
         private List<OrderItem> items;
         public IReadOnlyCollection<OrderItem> Items => items;
         public int TotalCount => items.Sum(item => item.Count);
 
-        public decimal TotalPrice => items.Sum(item => item.Price * item.Count) + Delivery?.Amount ?? 0m;
+        public decimal TotalPrice => items.Sum(item => item.Price * item.Count) + (Delivery?.Amount ?? 0m);
 
         public Order(int id, IEnumerable<OrderItem> items)
         {
